4bebbecd055ec5a1eea908bf30f3cec4c64f731b
Nikolay Varikash <n.varicash@gmail.com>
2025-06-29 17:54:24 +0300

    adjusted UI of the room

--- src/app/room/[id]/page.tsx
+++ src/app/room/[id]/page.tsx
@@ -21,6 +21,13 @@ export default function RoomPage() {
   const isHost = useIsHost();
   const { vote, resetVotes, isConnected } = usePokerStore();
 
+  const playerZones = {
+    top: users.slice(0, 4),
+    bottom: users.slice(4, 8),
+    left: users.slice(8, 10),
+    right: users.slice(10, 12)
+  }
+
   const handleVote = (value: VoteValue) => {
     if (!currentUser) return;
     vote(roomId, value);
@@ -40,21 +47,85 @@ export default function RoomPage() {
         userName={currentUser?.name || 'Unknown'}
         isConnected={isConnected}
       >
-        <Button variant='outline' size='sm'>Cписок участников</Button>
+        <Button variant='outline' size='sm'>Cписок участников ({users.length})</Button>
       </Header>
-    
-        <div>
-          <h2>Players ({users.length}):</h2>
-          {users.map((user) => (
-            <Player
-            key={user.id}
-            name={user.name}
-            vote={user.vote}
-            isRevealed={revealed}
-            isCurrentUser={user.id === currentUser?.id}
-          />
-          ))}
+
+      <div className={styles.main}>
+
+        <div className={styles.tableContainer}>
+
+          <div className={styles.topZone}>
+              {playerZones.top.map((user) => (
+                <Player
+                  key={user.id}
+                  name={user.name}
+                  vote={user.vote}
+                  isRevealed={revealed}
+                  isCurrentUser={user.id === currentUser?.id}
+                />
+              ))}
+          </div>
+
+          <div className={styles.middleSection}>
+            <div className={styles.leftZone}>
+                {playerZones.left.map((user) => (
+                  <Player
+                    key={user.id}
+                    name={user.name}
+                    vote={user.vote}
+                    isRevealed={revealed}
+                    isCurrentUser={user.id === currentUser?.id}
+                  />
+                ))}
+            </div>
+
+            <Table button={<Button>Show Cards</Button>}>
+            </Table>
+
+            <div className={styles.rightZone}>
+              {playerZones.right.map((user) => (
+                <Player
+                  key={user.id}
+                  name={user.name}
+                  vote={user.vote}
+                  isRevealed={revealed}
+                  isCurrentUser={user.id === currentUser?.id}
+                />
+              ))}
+            </div>
+          </div>
+
+          <div className={styles.bottomZone}>
+            {playerZones.bottom.map((user) => (
+              <Player
+                key={user.id}
+                name={user.name}
+                vote={user.vote}
+                isRevealed={revealed}
+                isCurrentUser={user.id === currentUser?.id}
+              />
+            ))}
+          </div>
+
         </div>
+
+        {!revealed && currentUser && (
+          <div>
+            <h2>Select your vote:</h2>
+            
+            <div className={styles.cardSection}>
+              {voteValues.map((value) => (
+                <Card
+                  key={value}
+                  value={value}
+                  size="sm"
+                  isRevealed={true}
+                  onClick={() => handleVote(value)}
+                />
+              ))}
+            </div>
+          </div>
+        )}    
 
         {isHost && revealed && (
           <div>
@@ -64,32 +135,8 @@ export default function RoomPage() {
           </div>
         )}
 
-        {!revealed && currentUser && (
-          <div>
-            <h2>Select your vote:</h2>
-            
-            <div>
-              {voteValues.map((value) => (
-                <Card
-                  key={value}
-                  value={value}
-                  size="sm"
-                  onClick={() => handleVote(value)}
-                />
-              ))}
-            </div>
-          </div>
-        )}
-
-        {revealed && (
-          <div>
-            <h2>All votes revealed!</h2>
-              <Button onClick={handleReset} disabled={!isHost}>
-                Reset Votes
-              </Button>
-          </div>
-        )}
-
+        
+      </div>
     </div>
   );
 } 
